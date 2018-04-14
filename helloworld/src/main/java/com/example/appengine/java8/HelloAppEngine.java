29cb8e961eb2a1502e9fe5e02629fc333c41792b
jmurray <jonte.murray@gmail.com>
2018-04-14 22:38:55 +0200

    Experiment with the endpoint

--- helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -1,35 +1,53 @@
 /**
  * Copyright 2017 Google Inc.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.example.appengine.java8;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "HelloAppEngine", value = "/hello")
 public class HelloAppEngine extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        response.setContentType("text/plain");
-        response.getWriter().println("Hello from the cloud!");
+  private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+
+    String query = request.getQueryString();
+
+    if (query == null) {
+      respond(response, ImmutableMap.of(
+          "error", "Please set a query in the URL.",
+          "result", "FAILURE"));
+    } else {
+      respond(response, ImmutableMap.of(
+          "query", query,
+          "result", "SUCCESS"));
     }
+  }
+
+  private void respond(HttpServletResponse response, Map<String, String> body) throws IOException {
+    response.getWriter().println(gson.toJson(body));
+  }
 }
--- helloworld/src/test/java/com/example/appengine/java8/HelloAppEngineTest.java
+++ helloworld/src/test/java/com/example/appengine/java8/HelloAppEngineTest.java
@@ -1,22 +1,27 @@
 /**
  * Copyright 2017 Google Inc.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.example.appengine.java8;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
+
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,59 +30,43 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.when;
-
-/**
- * Unit tests for {@link HelloAppEngine}.
- */
-// [START example]
 @RunWith(JUnit4.class)
 public class HelloAppEngineTest {
-    private static final String FAKE_URL = "fake.fk/hello";
-    // Set up a helper so that the ApiProxy returns a valid environment for local testing.
-    private final LocalServiceTestHelper helper = new LocalServiceTestHelper();
 
-    @Mock
-    private HttpServletRequest mockRequest;
-    @Mock
-    private HttpServletResponse mockResponse;
-    private StringWriter responseWriter;
-    private HelloAppEngine servletUnderTest;
+  // Set up a helper so that the ApiProxy returns a valid environment for local testing.
+  private final LocalServiceTestHelper helper = new LocalServiceTestHelper();
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        helper.setUp();
+  @Mock
+  private HttpServletRequest mockRequest;
+  @Mock
+  private HttpServletResponse mockResponse;
+  private StringWriter responseWriter;
+  private HelloAppEngine servlet;
 
-        //  Set up some fake HTTP requests
-        when(mockRequest.getRequestURI()).thenReturn(FAKE_URL);
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    helper.setUp();
 
-        // Set up a fake HTTP response.
-        responseWriter = new StringWriter();
-        when(mockResponse.getWriter()).thenReturn(new PrintWriter(responseWriter));
+    responseWriter = new StringWriter();
+    when(mockResponse.getWriter()).thenReturn(new PrintWriter(responseWriter));
 
-        servletUnderTest = new HelloAppEngine();
-    }
+    servlet = new HelloAppEngine();
+  }
 
-    @After
-    public void tearDown() {
-        helper.tearDown();
-    }
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
 
-    @Test
-    public void doGetWritesResponse() throws Exception {
-        servletUnderTest.doGet(mockRequest, mockResponse);
+  @Test
+  public void doGetWritesResponse() throws Exception {
+    when(mockRequest.getRequestURI()).thenReturn("some-host/hello");
+    when(mockRequest.getQueryString()).thenReturn("yo");
 
-        // We expect our hello world response.
-        assertThat(responseWriter.toString())
-                .named("HelloAppEngine response")
-                .isEqualTo("Hello from the cloud!\n");
-    }
+    servlet.doGet(mockRequest, mockResponse);
+
+    assertThat(responseWriter.toString()).isNotEmpty();
+    System.out.println(responseWriter.toString());
+  }
 }
-// [END example]
